e4cd442a89f26019b0b08b135061d2a977f96d64
Dusty404 <165195494+Dusty404@users.noreply.github.com>
2025-04-15 02:47:26 +0200

    worked on contact me form

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FooterComponent } from "../footer/footer.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-me',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FooterComponent, FormsModule],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.scss'
 })
 export class ContactMeComponent {
 
+  hoverMid:boolean = false;
+  hoverBot:boolean = false;
+  isChecked:boolean = false;
 }
