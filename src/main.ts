edbefbdb6bb10199dd84b58cf6043d52189c5fea
Dusty404 <165195494+Dusty404@users.noreply.github.com>
2025-08-20 15:04:08 +0200

    update to angular 18

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,11 @@ import { AppComponent } from './app/app.component';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app/app.routes';
 import { importProvidersFrom } from '@angular/core';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 const scrollConfig = {
   scrollPositionRestoration: 'top' as const,
@@ -19,19 +20,17 @@ export function HttpLoaderFactory(http: HttpClient) {
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(
-      routes,
-  withInMemoryScrolling({
-    scrollPositionRestoration: 'top',
-    anchorScrolling: 'enabled',
-  })
-),
+    provideRouter(routes, withInMemoryScrolling(scrollConfig)),
+    provideHttpClient(withFetch()), // 👈 statt HttpClientModule
     importProvidersFrom(
-      HttpClientModule,
       TranslateModule.forRoot({
-        loader: { provide: TranslateLoader, useFactory: HttpLoaderFactory, deps: [HttpClient] }
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient],
+        },
       })
     ),
     provideAnimationsAsync()
-  ]
+  ],
 }).catch((err) => console.error(err));
